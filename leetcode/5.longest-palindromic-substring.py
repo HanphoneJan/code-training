a1c07f1e5aec147dfd87e3f0486d0862d684e31e
HanphoneJan <1195560097@qq.com>
2026-03-03 22:49:01 +0800

    调整

---
+++ leetcode/5.longest-palindromic-substring.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=5 lang=python3
+# @lcpr version=30204
+#
+# [5] 最长回文子串
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# "babad"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "cbbd"\n
+# @lcpr case=end
+
+#
+
