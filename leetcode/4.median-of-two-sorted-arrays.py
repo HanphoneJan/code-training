776df6914ee3fcacb411413861948598f48b3675
寒枫 <1195560097@qq.com>
2026-03-04 18:26:23 +0800

    尝试提交

--- leetcode/4.median-of-two-sorted-arrays.py
+++ leetcode/4.median-of-two-sorted-arrays.py
@@ -12,7 +12,13 @@
 # @lc code=start
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
+        nums = nums1 + nums2
+        nums.sort()
+        mid = len(nums) // 2
+        if len(nums) % 2 == 0:
+            return (nums[mid - 1] + nums[mid]) / 2
+        else:
+            return nums[mid]
 # @lc code=end
 
 
