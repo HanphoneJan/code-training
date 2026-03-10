560b6df10a85f7f26255b6553ca1b9e83f104700
HanphoneJan <1195560097@qq.com>
2026-03-10 20:37:26 +0800

    更新leetcode5

--- leetcode/5.longest-palindromic-substring.py
+++ leetcode/5.longest-palindromic-substring.py
@@ -12,7 +12,28 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
         
+        def expandAroundCenter(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left + 1, right - 1
+        
+        start, end = 0, 0
+        for i in range(len(s)):
+            # 奇数长度回文串
+            l1, r1 = expandAroundCenter(i, i)
+            # 偶数长度回文串
+            l2, r2 = expandAroundCenter(i, i + 1)
+            
+            if r1 - l1 > end - start:
+                start, end = l1, r1
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+        
+        return s[start:end + 1]
 # @lc code=end
 
 
