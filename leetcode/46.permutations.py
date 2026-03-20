7e33a8e4ace8f5d3589db86d700541eff3c9f861
寒枫 <1195560097@qq.com>
2026-03-20 18:04:14 +0800

    46

--- leetcode/46.permutations.py
+++ leetcode/46.permutations.py
@@ -12,12 +12,36 @@
 # @lc code=start
 from typing import List
 
+# 回溯经典题目
+# 回溯定义：一种通过探索所有可能解，遇到死路就回退的算法思想。
+# def backtrack(路径, 选择列表):
+#     if 满足结束条件:
+#         收集结果
+#         return
+#     for 选择 in 选择列表:
+#         做选择                # 将选择加入路径
+#         backtrack(新路径, 新选择列表)
+#         撤销选择              # 将选择从路径中移除
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
+        group = []
+        n = len(nums)
+        visited = [False] * n 
 
         def dfs(nums:List[int]):
-            
+            if len(group)==n:
+                ans.append(group[:])
+                return
+            for i in range(n):
+                if not visited[i]:
+                    visited[i] = True
+                    group.append(nums[i])
+                    dfs(nums)
+                    group.pop()  #恢复group和visited，这就是恢复现场
+                    visited[i] = False
+        dfs(nums)
+        return ans
 # @lc code=end
 
 
---
+++ leetcode/51.n-queens.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode.cn id=51 lang=python3
+# @lcpr version=30204
+#
+# [51] N 皇后
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+from typing import List
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# 4\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 1\n
+# @lcpr case=end
+
+#
+
